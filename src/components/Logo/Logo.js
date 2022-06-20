4557c7b020362329839af0db63420931b052fcfe
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-06-19 20:23:17 -0400

    Logo Component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,3 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Tilt from "react-parallax-tilt";
+import "./Logo.css";
+import brain from "./brain100.png";
 
-const Logo = () => {};
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt
+                className="Tilt br2 shadow-2"
+                style={{
+                    height: "150px",
+                    width: "150px",
+                    backgroundColor: "blue",
+                    tiltMaxAngleX: "55",
+                    tiltMaxAngleY: "55",
+                }}
+            >
+                <div className="pa3 ">
+                    <img style={{ paddingTop: "5px" }} src={brain} alt="Logo" />
+                </div>
+            </Tilt>
+        </div>
+    );
+};
+
+export default Logo;

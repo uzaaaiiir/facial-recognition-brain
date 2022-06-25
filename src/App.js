b72cace37b40f0ba3a1c9b1d6a6e2ca882c011d9
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-06-24 22:21:37 -0400

    Particles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
-// import Particles from "react-tsparticles";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 class App extends Component {
     render() {
+        const particlesInit = async (main) => {
+            console.log(main);
+
+            // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            await loadFull(tsParticles);
+        };
+
+        const particlesLoaded = (container) => {
+            console.log(container);
+        };
         return (
             <div className="App">
+                <Particles className="particles" params={particlesOptions} />
+
                 <Navigation />
                 <Logo />
                 <Rank />

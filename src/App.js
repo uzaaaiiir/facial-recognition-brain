28dd12d4bbad17825ff82359b1614c0623b69a0d
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-06-26 20:47:56 -0400

    Trouble-shooting Particles.js

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import ParticlesBackground from "./components/ParticlesBackground/ParticlesBackground";
 
 class App extends Component {
     render() {
-        const particlesInit = async (main) => {
-            console.log(main);
-
-            // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-            // starting from v2 you can add only the features you need reducing the bundle size
-            await loadFull(tsParticles);
-        };
-
-        const particlesLoaded = (container) => {
-            console.log(container);
-        };
         return (
             <div className="App">
-                <Particles className="particles" params={particlesOptions} />
-
+                <ParticlesBackground />
                 <Navigation />
                 <Logo />
                 <Rank />
---
+++ src/components/ParticlesBackground/ParticlesBackground.js
@@ -0,0 +1,8 @@
+import Particles from "react-tsparticles";
+import particlesConfig from "./particles-config";
+
+const ParticlesBackground = () => {
+    return <Particles params={particlesConfig}></Particles>;
+};
+
+export default ParticlesBackground;

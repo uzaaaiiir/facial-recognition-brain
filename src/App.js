e60d5b5e2ca2c1a6848247cd755d12d65b565e96
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-06-27 22:43:36 -0400

    Updated Components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
-import ParticlesBackground from "./components/ParticlesBackground/ParticlesBackground";
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: "",
+        };
+    }
+
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    };
+
+    onButtonSubmit = () => {
+        console.log("click");
+    };
+
     render() {
         return (
             <div className="App">
-                <ParticlesBackground className="particles" />
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
                 {/*<FaceRecognition />*/}
             </div>
         );
